b6ffba133a366a897abeb641e6a7cbda8541b54d
Alexey <a.titov@doctis.ru>
2021-01-24 21:44:52 +0300

    update

---
+++ src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <Button label="test" />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Button from '@/stories/buttons/Button.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    Button
+  }
+}
+</script>
